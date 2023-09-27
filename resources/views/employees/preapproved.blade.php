29b25101a1180a8ab45577a8be836f0ef32ab543
shix13 <115773037+shix13@users.noreply.github.com>
2023-09-27 11:35:13 +0800

    Update preapproved.blade.php
    Changes to fix to modal deny

--- resources/views/employees/preapproved.blade.php
+++ resources/views/employees/preapproved.blade.php
@@ -62,7 +62,7 @@
         <br>
         @endif
         
-    <div class="card ">
+    <div class="card">
         <div class="card-header">
             <h4 class="card-title" style="font-weight: 700">Pre-Approved Booking</h4>
         </div>
@@ -70,27 +70,15 @@
             <div class="table-responsive">
                 <table id="table" class="table table-hover table-striped">
                     <thead class="text-primary font-montserrat">
-                        <th class="bold-text">
-                            <strong>#</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Booking ID</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Downpayment Fee</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Gcash Reference No.</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Subtotal</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Status</strong>
-                        </th>
-                        <th class="bold-text">
-                            <strong>Actions</strong>
-                        </th>
+                        <tr>
+                            <th class="bold-text"><strong>#</strong></th>
+                            <th class="bold-text"><strong>Booking ID</strong></th>
+                            <th class="bold-text"><strong>Downpayment Fee</strong></th>
+                            <th class="bold-text"><strong>Gcash Reference No.</strong></th>
+                            <th class="bold-text"><strong>Subtotal</strong></th>
+                            <th class="bold-text"><strong>Status</strong></th>
+                            <th class="bold-text"><strong>Actions</strong></th>
+                        </tr>
                     </thead>
                     <tbody>
                         @php
@@ -101,18 +89,44 @@
                             @foreach ($preApprovedBookings as $preApprovedBooking)
                                 <tr class="text-center">
                                     <td>{{ $counter++ }}</td>
-                                    <td><strong>{{ $preApprovedBooking->reserveID }}</strong></td>                                 
+                                    <td><strong>{{ $preApprovedBooking->reserveID }}</strong></td>
                                     <td>{{ $preApprovedBooking->downpayment_Fee }}</td>
                                     <td>{{ $preApprovedBooking->gcash_RefNum }}</td>
                                     <td>{{ $preApprovedBooking->subtotal }}</td>
                                     <td>{{ $preApprovedBooking->status }}</td>
-
                                     <td class="col-md-1">
                                         <a href="{{ route('employee.approveBooking', ['bookingId' => $preApprovedBooking->reserveID]) }}" class="btn btn-success btn-block" style="margin-bottom: 10px;"><strong><i class="fa-solid fa-check"></i> Approve</strong></a>
                                       
-                                            <a href="{{ route('employee.denyBooking', ['bookingId' => $preApprovedBooking->reserveID]) }}" class="btn btn-danger btn-block" style="margin-bottom: 10px;"><strong><i class="fa-solid fa-xmark"></i> Reject</strong></a>
-                                    </td>                                   
+                                        <!-- Trigger the modal for denying the booking -->
+                                        <button type="button" class="btn btn-danger btn-block" data-toggle="modal" data-target="#denyModal{{ $preApprovedBooking->reserveID }}"><strong><i class="fa-solid fa-xmark"></i> Reject</strong></button>
+                                    </td>
                                 </tr>
+
+                                <!-- Deny Modal -->
+                                <div class="modal fade" id="denyModal{{ $preApprovedBooking->reserveID }}" tabindex="-1" role="dialog" aria-labelledby="denyModalLabel{{ $preApprovedBooking->reserveID }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="denyModalLabel{{ $preApprovedBooking->reserveID }}">Confirm Denial</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <form method="POST" action="{{ route('employee.denyBooking', ['bookingId' => $preApprovedBooking->reserveID]) }}">
+                                                @csrf
+                                                @method('PUT')
+                                                <div class="modal-body">
+                                                    Are you sure you want to deny this booking?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal" style="margin-right: 5px;">Cancel</button>
+                                                    <!-- Add a submit button to trigger the denial process -->
+                                                    <button type="submit" class="btn btn-danger">Deny</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         @else
                             <tr>
