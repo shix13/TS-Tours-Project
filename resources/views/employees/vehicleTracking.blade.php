13edf3e01528f23bf66be34477862cf605d7f455
aeeeed <aedriellelalbina@su.edu.ph>
2023-10-30 23:49:26 +0800

    Vehicle tracking date format
    minor change to vehicle tracking's date format for "last sent"

--- resources/views/employees/vehicleTracking.blade.php
+++ resources/views/employees/vehicleTracking.blade.php
@@ -17,17 +17,26 @@
                     @php
                         $vehicle = $assignment->vehicle;
                     @endphp
-                    <div class="card custom-card mb-4" style="height:220px;">
-                        <div class="row g-0">
+                    <div class="card">
+                        <!--div class="row g-0"-->
+                        <div style="height: 100px; overflow: hidden; margin: 0 auto;">
+                        <img src="{{ asset('storage/' . $vehicle->pic) }}" class="card-img-top" style="width: 100%; height: 100%; object-fit: cover;" alt="Vehicle Image">
+                        </div>
                             <!--div class="col-md-4">
                                 <img src="{{ asset('storage/' . $vehicle->pic) }}" style="width: 100%; height: 100%; object-fit: cover;" class="img-fluid rounded-start" alt="Vehicle Image">
                             </div-->
-                            <div class="col">
+                            <!--div class="col"-->
                                 <div class="card-body">
-                                    <h5 class="card-title">{{ $vehicle->unitName }} - {{ $vehicle->registrationNumber }}</h5>
+                                    <h5 class="card-title" style="text-align:center;"><b>{{ $vehicle->unitName }} - {{ $vehicle->registrationNumber }}</b></h5>
                                     <p class="card-text">
-                                        Driver: {{ $assignment->employee->firstName }} {{ $assignment->employee->lastName }}<br/>
-                                        BookingID: {{ $assignment->rent->reserveID }}<br/>
+                                        <div class="row">
+                                            <div class="col" style="text-align:center">
+                                                <b>Driver</b> <br/>{{ $assignment->employee->firstName }} {{ $assignment->employee->lastName }}
+                                            </div>
+                                            <div class="col" style="text-align:center">
+                                                <b>BookingID</b> <br/>{{ $assignment->rent->reserveID }}
+                                            </div>
+                                        </div>
                                         <hr>
                                         <div class="geolocationData" data-assignment-id="{{ $assignment->assignedID }}">
                                             @php
@@ -40,8 +49,8 @@
                                                     $dateCarbon = \Carbon\Carbon::parse($dateString);
                                                     $date = $dateCarbon->format('F, j, Y g:i A');
                                                 @endphp
-                                                Latitude: {{ $location->latitude }} <br/>
-                                                Longitude: {{ $location->longitude }} <br/> 
+                                                Latitude: {{ $location->latitude }}<br/>
+                                                Longitude: {{ $location->longitude }}<br/>
                                                 Last Sent: {{ $date }}<br/>
                                             @else
                                                 Location not yet sent
@@ -49,8 +58,8 @@
                                         </div>
                                     </p>
                                 </div>    
-                            </div>
-                        </div>
+                            <!--/div-->
+                        <!--/div-->
                     </div>
                     </div>
             @endforeach
@@ -73,10 +82,19 @@ function updateGeolocationData(assignmentId) {
             if (data.data) {
                 var location = data.data;
                 var dateCarbon = new Date(location.created_at);
+                var options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true
+                };
 
                 var content = 'Latitude: ' + location.latitude + '<br/>' +
                             'Longitude: ' + location.longitude + '<br/>' +
-                            'Last Sent: ' + dateCarbon.toLocaleString() + '<br/>';
+                            'Last Sent: ' + dateCarbon.toLocaleString(undefined, options) + '<br/>';
 
                 geolocationContainer.html(content);
                 console.log("update!");
